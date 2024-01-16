a08cad892a20789c012dead7d2a80bef10fa0fe3
hewitt08 <143777083+hewitt08@users.noreply.github.com>
2024-01-16 11:12:19 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -28,9 +28,9 @@ public void setup(){
     }else{
       count = 0;
     }
-  }
+  }*/
 }
-*/
+
 public boolean checkPrime(double num){
   if(num<2){return false;}
   for(int i = 2; i <= Math.sqrt(num); i++){
